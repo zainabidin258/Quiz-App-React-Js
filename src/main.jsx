1426339448c7c27fd188eccf1987fd293977b8ad
Zain <zainofficial258@gmail.com>
2023-09-05 05:49:22 +0500

    Starting Up

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import QuizApp from '../components/QuizApp'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QuizApp/>
+  </React.StrictMode>,
+)
