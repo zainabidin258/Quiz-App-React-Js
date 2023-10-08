83e0afb340b08bf554b5f51024fbd30362d57093
Zain <zainofficial258@gmail.com>
2023-10-09 00:22:21 +0500

    Final Commit

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -2,64 +2,142 @@ import React from 'react'
 import { useState , useContext } from 'react'
 import { QuizContext } from '../Helpers/Context';
 import { Questions } from '../Helpers/Questions'
+import "../Styles/App.css"
+
+
 
 function Quiz() {
-  const { score , setScore, setGameState }  = useContext(QuizContext);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [optionChosen, setOptionChosen] = useState('');
+  const [optionChosen, setOptionChosen] = useState("");
+
+  const { score, setScore , setGameState } = useContext(
+    QuizContext
+  );
+
+  const chooseOption = (option) => {
+    setOptionChosen(option);
+  };
 
   const nextQuestion = () => {
-    if(Questions[currentQuestion].asnwer == optionChosen){
-      setScore(score + 1)
+    if (Questions[currentQuestion].asnwer == optionChosen) {
+      setScore(score + 1);
     }
-    setCurrentQuestion(currentQuestion + 1)
-  }
+    setCurrentQuestion(currentQuestion + 1);
+  };
 
   const finishQuiz = () => {
-    if(Questions[currentQuestion].asnwer == optionChosen){
-      setScore(score + 1)
+    if (Questions[currentQuestion].asnwer == optionChosen) {
+      setScore(score + 1);
     }
-    setGameState('result')
-  }
+    setGameState("result");
+  };
+
   return (
-    <>
-      <div className='Quiz'>
-      <h3>
-        {Questions[currentQuestion].prompt}
-      </h3>
-      <div className="options">
-        <button 
-          onClick={() => setOptions('A')}
+    <div className="Quiz">
+      <h1>{Questions[currentQuestion].prompt}</h1>
+      <div className="questions">
+        <button
+          onClick={() => {
+            chooseOption("optionA");
+          }}
         >
           {Questions[currentQuestion].optionA}
         </button>
-                <button 
-          onClick={() => setOptions('B')}
+        <button
+          onClick={() => {
+            chooseOption("optionB");
+          }}
         >
           {Questions[currentQuestion].optionB}
         </button>
-                <button 
-          onClick={() => setOptions('C')}
+        <button
+          onClick={() => {
+            chooseOption("optionC");
+          }}
         >
           {Questions[currentQuestion].optionC}
         </button>
-                <button 
-          onClick={() => setOptions('D')}
+        <button
+          onClick={() => {
+            chooseOption("optionD");
+          }}
         >
           {Questions[currentQuestion].optionD}
         </button>
       </div>
-      {
-        currentQuestion == Questions.length -1 ? (
-          <button onClick={finishQuiz}>Finish</button>
-        ) : (
-          <button onClick={nextQuestion}>Next</button>
-        ) 
-      }
-        
-      </div>
-    </>
-  )
+
+      {currentQuestion == Questions.length - 1 ? (
+        <button onClick={finishQuiz} id="nextQuestion">
+          Finish Quiz
+        </button>
+      ) : (
+        <button onClick={nextQuestion} id="nextQuestion">
+          Next Question
+        </button>
+      )}
+    </div>
+  );
 }
 
-export default Quiz
+export default Quiz;
+// function Quiz() {
+//   const { score , setScore, setGameState }  = useContext(QuizContext);
+//   const [currentQuestion, setCurrentQuestion] = useState(0);
+//   const [optionChosen, setOptionChosen] = useState('');
+
+  
+//   const nextQuestion = () => {
+//     if(Questions[currentQuestion].asnwer == optionChosen){
+//       setScore(score + 1)
+//     }
+//     setCurrentQuestion(currentQuestion + 1)
+//   }
+
+//   const finishQuiz = () => {
+//     if(Questions[currentQuestion].asnwer == optionChosen){
+//       setScore(score + 1)
+//     }
+//     setGameState('result')
+//   }
+//   return (
+//     <>
+//       <div className='Quiz'>
+//       <h3>
+//         {Questions[currentQuestion].prompt}
+//       </h3>
+//       <div className="options">
+//         <button 
+//           onClick={() => setOptions('A')}
+//         >
+//           {Questions[currentQuestion].optionA}
+//         </button>
+//                 <button 
+//           onClick={() => setOptions('B')}
+//         >
+//           {Questions[currentQuestion].optionB}
+//         </button>
+//                 <button 
+//           onClick={() => setOptions('C')}
+//         >
+//           {Questions[currentQuestion].optionC}
+//         </button>
+//                 <button 
+//           onClick={() => setOptions('D')}
+//         >
+//           {Questions[currentQuestion].optionD}
+//         </button>
+//       </div>
+//       {
+//         currentQuestion == Questions.length -1 ? (
+//           <button onClick={finishQuiz}>Finish</button>
+//         ) : (
+//           <button style={{background:'lightblue'}} onClick={nextQuestion}>Next</button>
+//         ) 
+//       }
+        
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Quiz
